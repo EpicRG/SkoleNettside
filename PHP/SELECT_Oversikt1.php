0a3b7afcf5ddce4fc8217c5fd36c20cf17ad1e01
EpicRG <epicrggaming@gmail.com>
2021-05-25 15:39:13 +0200

    tries to fixs database

--- PHP/SELECT_Oversikt1.php
+++ PHP/SELECT_Oversikt1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Database</title>
     <link rel="stylesheet" type="text/css" href="../CSS/Oversikt.css">
 </head>
 <body>
@@ -14,7 +14,7 @@
  $tjener = "localhost";
  $brukernavn = "root";
  $passord = "";
- $database = "mydb";
+ $database = "mysql";
  // Opprette en kobling
  $kobling = new mysqli($tjener, $brukernavn, $passord, $database);
  // Sjekk om koblingen virker
